9bc53f39a54c327a946832c2e29333b3549bc244
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-25 13:38:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -8,7 +8,7 @@ class Owner
   
   def initialize(name = "Owner")
     @owner = self
-  #  @name = name 
+    @name = name 
   #  self.name = @name
     @pets = {:dogs => [], :cats => []}
    # save 
