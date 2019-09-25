cf1ecb253ad79d777f6db845d679342c7b8b4a15
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-25 14:56:57 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -39,11 +39,11 @@ class Owner
   end 
   
   def cats 
-    @cats = Cat.all.select { |cat| cat.owner == self }
+    @cats = Cat.all.select { |cat| cat.owner = self }
   end 
   
   def dogs 
-    @dogs = Dog.all.select { |dog| dog.owner == self }
+    @dogs = Dog.all.select { |dog| dog.owner = self }
   end 
   
   def buy_cat(cat_name)
