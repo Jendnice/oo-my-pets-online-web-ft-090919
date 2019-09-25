83b9ba970173580e4dcea6b95874437bcdb1404a
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-25 13:00:53 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -7,7 +7,7 @@ class Owner
   @@all = []
   
   def initialize(name = "Owner")
-    @owner = owner(@name = name)
+    @owner = owner.(@name = name)
     @pets = {:dogs => [], :cats => []}
     save 
   end 
