d31035d310198e7c6200fcb533a2abf9f7194bf4
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-25 12:15:53 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,56 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_accessor 
+  attr_reader :name, :species
+  
+  @@all = []
+  @pets = []
+  
+  def initialize(name = "Owner")
+    @name = name
+    save 
+  end 
+  
+  def save
+    @@all << self 
+  end 
+  
+  def species
+    @species = "human" 
+  end 
+  
+  def say_species
+    "I am a #{species}."
+  end 
+  
+  def self.all 
+    @@all.uniq 
+  end 
+  
+  def self.count
+    self.all.count 
+  end 
+  
+  def self.reset_all 
+    @@all.clear
+  end 
+  
+  def cats 
+    @cats = Cat.all.select { |cat| cat.owner == self }
+  end 
+  
+  def dogs 
+    @dogs = Dog.all.select { |dog| dog.owner == self }
+  end 
+  
+  def buy_cat(cat)
+    cat.owner = self 
+    @pets << cat 
+  end
+  
+  
 end
+
+
+ 
